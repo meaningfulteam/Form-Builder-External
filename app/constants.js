eeec47009db55d7c878b809af412f54c30d23e7f
juan <juan@meaningfulteam.com>
2024-05-04 15:18:21 -0400

    feat: Add the posibility to handle multiple phone fields in one screen

--- app/constants.js
+++ app/constants.js
@@ -6,7 +6,8 @@ export const screenTypes = { fields: "fields", radio: "radio", check: "checkbox"
 export const elementAttr = `${toolPrefix}-el`;
 export const screenTypeAttr = `${toolPrefix}-screen-type`;
 export const interactedAttr = `${toolPrefix}-interacted`;
-export const disabledAttr = `${toolPrefix}-disabled`
+export const disabledAttr = `${toolPrefix}-disabled`;
+export const inputWrapperAttr = `${toolPrefix}-phone-wrapper`;
 
 // Classes
 export const errorClass = "m8l-error-message";
